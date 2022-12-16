9d80605ede8f74306d9615e65320c95c3483e9a8
salli <salli@MSI>
2022-12-16 11:10:56 +0300

    updated

---
+++ Services/Catalog/Catalog/Settings/IDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Settings
+{
+    public interface IDatabaseSettings
+    {
+        public string CourseCollectionName { get; set; }
+        public string CategoryCollectionName { get; set; }
+        public string CoonectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
