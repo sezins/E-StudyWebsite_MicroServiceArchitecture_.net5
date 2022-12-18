00301270247b6253781c99f5f20648d35280e445
salli <salli@MSI>
2022-12-18 20:28:10 +0300

    .

---
+++ Services/Catalog/Catalog/Services/CouseService.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Catalog.Models;
+using MongoDB.Driver;
+
+namespace Catalog.Services
+{
+    public class CouseService
+    {
+        private readonly IMongoCollection<Course> _courseCollection;
+
+        private readonly IMapper _mapper;
+        public CouseService()
+        {
+
+        }
+    }
+}
