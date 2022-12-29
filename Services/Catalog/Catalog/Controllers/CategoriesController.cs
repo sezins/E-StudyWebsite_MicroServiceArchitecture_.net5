6853f76411717924301f7d0f2dc3fbda40e02b2b
salli <sezininyayla@gmail.com>
2022-12-29 14:17:25 +0300

    updated

---
+++ Services/Catalog/Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,40 @@
+﻿using Catalog.Dtos;
+using Catalog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.ControllerBases;
+using System.Threading.Tasks;
+
+namespace Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            return CreateActionResultInstance(categories);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(category);
+        }
+
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+           var response = await _categoryService.CreateAsync(categoryDto);
+            return CreateActionResultInstance(response);
+        }
+
+    }
+}
--- Services/Catalog/Catalog/Controllers/CoursesController.cs
+++ Services/Catalog/Catalog/Controllers/CoursesController.cs
@@ -1,24 +1,68 @@
-﻿using Catalog.Services;
+﻿using Catalog.Dtos;
+using Catalog.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Shared.ControllerBases;
 using System.Threading.Tasks;
 
 namespace Catalog.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    internal class CoursesController : ControllerBase
+    internal class CoursesController : CustomBaseController
     {
         private readonly ICourseService _courseService;
-        public CoursesController(ICourseService courseService)
+        internal CoursesController(ICourseService courseService)
         {
             _courseService = courseService;
         }
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _courseService.GetAllAsync();
+            return CreateActionResultInstance(response);
+
+        }
+
+        [HttpGet("{id}")]
         public async Task<IActionResult> GetById(string id) 
         {
             var response=await _courseService.GetByIdAsync(id);
-
-            
+            return CreateActionResultInstance(response);            
         }
+
+        //api/courses/getallbyuserid/4
+        [Route("/api/[controller]/GetAllByUserId/{userId}")]
+        public async Task<IActionResult> GetAllByUserId(string userId)
+        {
+            var response = await _courseService.GetAllByUserId(userId);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateDto courseCreateDto)
+        {
+            var response = await _courseService.GetAllAsync();
+            return CreateActionResultInstance(response);
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(CourseUpdateDto courseUpdateDto)
+        {
+            var response = await _courseService.UpdateAsync(courseUpdateDto);
+            return CreateActionResultInstance(response);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _courseService.DeleteAsync(id);
+            return CreateActionResultInstance(response);
+
+        }
+
+
     }
 }
